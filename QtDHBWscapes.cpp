0de232e1259153bda4fd106c6bdfefe435c6967e
Bernd Dorer <Bernd.Dorer@rena.com>
2021-07-07 08:08:50 +0200

    Farbe einstellen

--- QtDHBWscapes.cpp
+++ QtDHBWscapes.cpp
@@ -1,8 +1,95 @@
 #include "QtDHBWscapes.h"
+#include <time.h>
 
 QtDHBWscapes::QtDHBWscapes(QWidget *parent)
     : QMainWindow(parent)
 {
+    srand(time(NULL));
     ui.setupUi(this);
-    ui.pushButton->setStyleSheet("border-image:url(D://DHBW//Privat//gehirn.png);");
+    ui.Button_1_1->setStyleSheet(blau);
+    ui.Button_1_2->setStyleSheet(bombe);
+    ui.Button_1_3->setStyleSheet(disco);
+    ui.Button_1_4->setStyleSheet(gelb);
+    ui.Button_1_5->setStyleSheet(gruen);
+    ui.Button_1_6->setStyleSheet(hellblau);
+    ui.Button_1_7->setStyleSheet(horizontal);
+    ui.Button_1_8->setStyleSheet(pink);
+    ui.Button_1_9->setStyleSheet(rot);
+    ui.Button_1_10->setStyleSheet(vertikal);
+    
+
+    for (int x = 1; x < 13; x++)
+    {
+        for (int y = 1; y < 13; y++)
+        {
+            int color = rand() % 6 + 1;
+            ColorButton(color, y, x);
+            
+        }
+    }
+}
+
+void  QtDHBWscapes::ColorButton(int color, int y, int x)
+{
+    QString Button = "Button_";
+    if (x < 10)
+    {
+        Button.append(x + 48); //append(std::to_string(x));
+    }
+    else
+    {
+        Button.append("1");
+        Button.append((x - 10) + 48);
+    }
+    Button.append("_");
+    if (y < 10)
+    {
+        Button.append(y + 48);
+    }
+    else
+    {
+        Button.append("1");
+        Button.append((y-10) + 48);
+    }
+    
+    QPushButton* bt = ui.gridLayoutWidget->findChild<QPushButton*>(Button);
+    if (bt != NULL)
+    {
+        switch (color)
+        {
+        case 1:
+            bt->setStyleSheet(rot);
+            break;
+        case 2:
+            bt->setStyleSheet(blau);
+            break;
+        case 3:
+            bt->setStyleSheet(hellblau);
+            break;
+        case 4:
+            bt->setStyleSheet(gruen);
+            break;
+        case 5:
+            bt->setStyleSheet(pink);
+            break;
+        case 6:
+            bt->setStyleSheet(gelb);
+            break;
+        case 7:
+            bt->setStyleSheet(disco);
+            break;
+        case 8:
+            bt->setStyleSheet(bombe);
+            break;
+        case 9:
+            bt->setStyleSheet(horizontal);
+            break;
+        case 10:
+            bt->setStyleSheet(vertikal);
+            break;
+        default:
+            break;
+        }
+        
+    }
 }
--- QtDHBWscapes.h
+++ QtDHBWscapes.h
@@ -3,12 +3,25 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_QtDHBWscapes.h"
 
+// Spielsteine definieren
+#define rot "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//davis_rm.png);"
+#define blau "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//blau_rm.png);"
+#define hellblau "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//hellblau_rm.png);"
+#define gruen "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//gruen_rm.png);"
+#define pink "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//pink_rm.png);"
+#define gelb "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//gelb_rm.png);"
+#define disco "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//disco_rm.png);"
+#define bombe "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//bombe_rm.png);"
+#define horizontal "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//horizontal_rm.png);"
+#define vertikal "border-image:url(D://DHBW//2. Semester//06_Programmieren//01_Programmieraufgabe//04_Bilder//Ohne Hintergrund//vertikal_rm.png);"
+
 class QtDHBWscapes : public QMainWindow
 {
     Q_OBJECT
 
 public:
     QtDHBWscapes(QWidget *parent = Q_NULLPTR);
+    void ColorButton(int color, int y, int x);
 
 private:
     Ui::QtDHBWscapesClass ui;
