30ff63e3d947ab678b0f454905bda6174ee8f415
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2021-07-09 10:27:56 +0200

    Kommentare geadded

--- QtDHBWscapes.cpp
+++ QtDHBWscapes.cpp
@@ -122,28 +122,28 @@ void  QtDHBWscapes::ColorButton(int color, int y, int x)
 QtDHBWscapes::~QtDHBWscapes()
 {
 	killTimer(timerId);
-	//delete ui; klappt net
 }
 
-void QtDHBWscapes::timerEvent(QTimerEvent* event)//Update
+void QtDHBWscapes::timerEvent(QTimerEvent* event)//Is executed everytime the timer triggers
 {
 	std::string str;
 	qDebug();
-	if (((Sec % 60) < 10) && (Sec > 0)) // For adding an additional zero when its smaller than 10
+	if (((Sec % 60) < 10) && (Sec > 0)) // For adding an additional zero when its smaller than 10, just visual purpose
 	{
 		str = ("<html><head/><body><p><span style =\"font-size:18pt;\">" + std::to_string(Sec / 60) + ":0" + std::to_string(Sec % 60) + "</span></p></body></html>");
 	}
 
 	else if (Sec == 0) // Time is up
 	{
-		ui.PopUp->setStyleSheet("border-image:url(://QtDHBWscapes//Ohne Hintergrund//TimesUp.png);");
-		str = "<html><head/><body><p><span style =\"font-size:18pt;\">Time is up !</span></p></body></html>";
-		killTimer(timerId);
+		ui.PopUp->setStyleSheet("border-image:url(://QtDHBWscapes//Ohne Hintergrund//TimesUp.png);"); // loads a picture
+		str = "<html><head/><body><p><span style =\"font-size:18pt;\">Time is up !</span></p></body></html>"; //Displays that the time is up
+		killTimer(timerId); // kill Timer
 	}
-	else
+	else //creating an array which is also the stylesheet for the Label
 	{
 		str =("<html><head/><body><p><span style =\"font-size:18pt;\">" +  std::to_string(Sec / 60) + ":" + std::to_string(Sec % 60) + "</span></p></body></html>");
 	}
+    //Setting the Label, so that it Displays the Time and also adjusts the Progress bar so that it shows the right percentage
 	QString qstr = QString::fromStdString(str);
 	ui.displayTime->setText(qstr);
 	ui.timeBar->setValue((double)Sec / (double)dur * 100);
